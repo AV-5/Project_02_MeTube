e8dd5cc41fa0c70149500aa6b1169defff5a8981
AV <mvav1998@gmail.com>
2025-12-16 20:51:26 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+const app=express();
+app.use(
+    cors({
+        origin:process.env.CORS_ORIGIN,
+        credentials:true
+    })
+)
+//commom middleware
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+
+//import routes
+import healthcheckRouter from "./routes/healthcheckRoutes.js"
+//routes
+app.use("/api/v1/healthcheck",healthcheckRouter)
+export {app}
---
+++ src/controllers/healthcheckController.js
@@ -0,0 +1,9 @@
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {Asynchandler} from "../utils/AsyncHandler.js"
+
+const healthcheck=Asynchandler(async(req,res)=>{
+    return res
+        .status(200)
+        .json(new ApiResponse(200,"OK","Heathcheck Pass"))
+})
+export {healthcheck}
